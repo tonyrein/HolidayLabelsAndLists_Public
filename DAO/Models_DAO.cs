aac848bf3d5b6853510f6f4c7192727b6f6e86af
trein <crjcvg@gmail.com>
2018-07-29 16:42:33 -0400

    Added Models_DAO.cs

---
+++ DAO/Models_DAO.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class BagLabelInfo_DAO
+    {
+        public int Id { get; set; }
+        public int year { get; set; }
+        public string family_id { get; set; }
+        public string family_name { get; set; }
+        public string family_members { get; set; }
+        public string request_type { get; set; }
+        public string donor_code { get; set; }
+        public string donor_name { get; set; }
+    }
+}
+
+public class Donor_DAO
+{
+    public int Id { get; set; }
+    public string code { get; set; }
+    public string name { get; set; }
+}
+
+public class GiftLabelInfo_DAO
+{
+    public int Id { get; set; }
+    public int year { get; set; }
+    public string family_id { get; set; }
+    public string family_name { get; set; }
+    public string child_name { get; set; }
+    public string child_gender { get; set; }
+    public int child_age { get; set; }
+    public string request_type { get; set; }
+    public string request_detail { get; set; }
+    public string donor_code { get; set; }
+    public string donor_name { get; set; }
+}
+
+public class ServicesHouseholdEnrollment_DAO
+{
+    public int Id { get; set; }
+    public string service_type { get; set; }
+    public int year { get; set; }
+    public int month { get; set; }
+    public int day { get; set; }
+    public string head_of_household { get; set; }
+    public string family_id { get; set; }
+    public string phone { get; set; }
+    public string address { get; set; }
+    public string city { get; set; }
+    public string state_or_province { get; set; }
+    public string postal_code { get; set; }
+    public int gift_card_count { get; set; }
+}
